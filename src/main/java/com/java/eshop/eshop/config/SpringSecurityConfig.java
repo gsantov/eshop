0feef9ffef8a069f692cda5e50f3a16f4125a6ba
Genaro Sulca <gsulca@krugercorporation.com>
2023-12-24 17:12:13 -0500

    first config

--- src/main/java/com/java/eshop/eshop/config/SpringSecurityConfig.java
+++ src/main/java/com/java/eshop/eshop/config/SpringSecurityConfig.java
@@ -4,14 +4,11 @@ import com.java.eshop.eshop.services.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,23 +30,6 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                    .antMatchers("/api/v1/shop", "api/v1/product", "/api/v1/login" )
-//                    .permitAll()
-//                    .anyRequest()
-//                    .authenticated()
-//                    .and()
-////                .formLogin()
-////                    .loginPage("/api/v1/login")
-////                    .permitAll()
-////                    .and()
-//                .logout()
-//                    .permitAll();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
@@ -60,25 +40,14 @@ public class SpringSecurityConfig {
                     .anyRequest()
                     .authenticated()
                     .and()
-//                .formLogin()
-//                    .loginPage("/api/v1/login")
-//                    .permitAll()
-//                    .and()
                 .logout()
                     .permitAll()
                     .and()
                 .httpBasic()
                 .authenticationEntryPoint(new NoPopupBasicAuthenticationEntryPoint());
-//                    .loginProcessingUrl("/login")
-//                    .usernameParameter("username")
-//                    .passwordParameter("password")
-////                    .successHandler(successHandler())
-////                    .failureHandler(failureHandler())
-//                    .permitAll();
         return http.build();
     }
 
-//    @Bean
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService)
                 .passwordEncoder(passwordEncoder());
--- src/main/java/com/java/eshop/eshop/controllers/LoginController.java
+++ src/main/java/com/java/eshop/eshop/controllers/LoginController.java
@@ -1,63 +1,22 @@
 package com.java.eshop.eshop.controllers;
 
-import com.java.eshop.eshop.config.SpringSecurityConfig;
-import com.java.eshop.eshop.dto.OrderDTO;
 import com.java.eshop.eshop.dto.ResponseDTO;
 import com.java.eshop.eshop.dto.UserDTO;
 import com.java.eshop.eshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
 @RequestMapping("/api/v1/login")
 public class LoginController {
 
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-
     @Autowired
     private UserService userService;
-
-//    @PostMapping("user")
-//    public Principal user(Principal user) {
-//        return user;
-//    }
-//
-//    @PostMapping
-//    public String login(@RequestBody UserDTO userDTO) {
-////        try {
-////            passwordEncoder.encode(userDTO.getPassword());
-////            Authentication authenticate = authenticationManager.authenticate(
-////                    new UsernamePasswordAuthenticationToken(
-////                            userDTO.getUserName(),
-////                            userDTO.getPassword()
-////                    )
-////            );
-////
-////            // If authentication is successful, you can return a JWT or a success message
-////            return new ResponseEntity<>(true, HttpStatus.OK);
-////        } catch (AuthenticationException e) {
-////            // Handle authentication failure
-////            return new ResponseEntity<>(false, HttpStatus.OK);
-////        }
-//        return "login";
-//    }
 
     @PostMapping("/user")
     public ResponseEntity<ResponseDTO<String>> getUser(@RequestBody UserDTO userDTO) {
